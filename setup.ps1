bbc50a96682b7e2f3df5e474a4bf640080ed78de
lpraju <102536717+lpraju@users.noreply.github.com>
2023-06-05 15:23:19 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
-New-Item -Path 'c:\tits\' -Name 'paapps' -ItemType 'directory'
+New-Item -Path 'c:\kits\' -Name 'apps' -ItemType 'directory'
 # C:\Windows\System32\Sysprep\sysprep.exe
 # Start-Process -FilePath "C:\Windows\System32\sysprep\sysprep.exe" -ArgumentList "/oobe", "/generalize", "/shutdown" -Verb runAs
